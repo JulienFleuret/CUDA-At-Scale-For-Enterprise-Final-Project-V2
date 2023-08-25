e107dfa6e71b74431526eb63e067a7630ad264a0
julien-richard-fleuret <julien.fleuret@gmail.com>
2023-08-25 17:01:49 -0400

    fix github action issue

--- run.sh
+++ run.sh
@@ -2,4 +2,15 @@ if [ ! -d build ]; then
 mkdir build
 fi
 
-cd build && cmake .. -DUNIT_TEST=ON && make -j && ctest -R run_unit_test -V
+cd build
+
+if [ $# -eq 0 ]
+  then
+    cmake .. -DUNIT_TEST=ON
+  else
+    cmake .. -DUNIT_TEST=ON $1
+fi
+
+make -j && ctest -R run_unit_test -V
+
+
