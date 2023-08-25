36187040cb4fda62d61d332a777aa95fa9d76c31
julien-richard-fleuret <julien.fleuret@gmail.com>
2023-08-25 15:19:57 -0400

    add verbose mode for ctest

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ if [ ! -d build ]; then
 mkdir build
 fi
 
-cd build && cmake .. -DUNIT_TEST=ON && make -j && ctest -R run_unit_test
+cd build && cmake .. -DUNIT_TEST=ON && make -j && ctest -R run_unit_test -V
