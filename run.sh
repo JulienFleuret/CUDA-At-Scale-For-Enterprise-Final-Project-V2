2d387c39f519cb5a507e31cc683517e68582608e
julien-richard-fleuret <julien.fleuret@gmail.com>
2023-08-25 21:15:15 -0400

    no actions because no gpu on github

--- run.sh
+++ run.sh
@@ -6,11 +6,11 @@ cd build
 
 if [ $# -eq 0 ]
   then
-    cmake .. -DUNIT_TEST=ON
+	make -j && ctest -R run_unit_test -V && cmake .. -DUNIT_TEST=ON
   else
-    cmake .. -DUNIT_TEST=ON $1
+
 fi
 
-make -j && ctest -R run_unit_test -V
+
 
 
